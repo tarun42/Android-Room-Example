dc607431b2e351540e801cfdb73c3563a6815550
tarun42 <dastarun42@gmail.com>
2020-09-04 18:43:45 +0530

    ui design

--- app/src/main/java/com/example/android_room_example/ui/MainActivity.kt
+++ app/src/main/java/com/example/android_room_example/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android_room_example
+package com.example.android_room_example.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_room_example.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
