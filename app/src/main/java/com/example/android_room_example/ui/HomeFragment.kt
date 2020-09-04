794adc6d20dd079f6d678a38b785751988c55f80
tarun42 <dastarun42@gmail.com>
2020-09-04 19:42:22 +0530

    added fragments

--- app/src/main/java/com/example/android_room_example/ui/HomeFragment.kt
+++ app/src/main/java/com/example/android_room_example/ui/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.android_room_example.ui
 
+import android.net.wifi.hotspot2.pps.HomeSp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.android_room_example.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
@@ -19,5 +21,12 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        btn_add.setOnClickListener {
+
+        }
+
+    }
 }
