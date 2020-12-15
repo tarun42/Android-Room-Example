5f0f4be205bf0845397b8d5f1c4bafa50188c5a2
Tarun Medtiya <dastarun42@gmail.com>
2020-12-16 00:26:39 +0530

    Trial (#4)
    * trial_final

--- app/src/main/java/com/example/android_room_example/ui/MainActivity.kt
+++ app/src/main/java/com/example/android_room_example/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.android_room_example.R
 import com.google.android.material.internal.NavigationMenu
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
