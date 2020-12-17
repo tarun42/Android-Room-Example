fcb0012a1c3876549c8944438596bf1d58556f3e
tarun42 <dastarun42@gmail.com>
2020-12-17 12:51:32 +0530

    credential

--- app/src/main/java/com/example/android_room_example/database/NoteDatabase.kt
+++ app/src/main/java/com/example/android_room_example/database/NoteDatabase.kt
@@ -6,9 +6,10 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
+
     entities = [Note::class],
     version = 1
-)
+ )
 abstract class NoteDatabase : RoomDatabase() {
 
     abstract fun getNoteDao() : NoteDoa
